e04065c154201febf076c6d1734bdb182d2145a6
bukbunny <khomsan_su@cmu.ac.th>
2024-03-03 22:51:43 +0700

    add login system

---
+++ chatbot_back/api/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const prefix = "/api/v1";
+// const user = require("./api/user");
+const cmuOAuth = require("./cmuOAuth");
+// const cpe = require("./api/cpe");
+// const course = require("./api/course");
+// const student = require("./api/student");
+// const scores = require("./api/scores");
+// const admins = require("./api/admins");
+
+// const dbConnect = require("./db/dbConnect");
+// dbConnect();
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({limit: '50mb'}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(`${prefix}/cmuOAuth`, cmuOAuth);
+// app.use(`${prefix}/cpe`, cpe);
+// app.use(`${prefix}/user`, user);
+// app.use(`${prefix}/course`, course);
+// app.use(`${prefix}/student`, student);
+// app.use(`${prefix}/scores`, scores);
+// app.use(`${prefix}/admins`, admins);
+
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port}!`)
+);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  app.set("socket", socket);
+  //console.log(`${socket.id}: a user connected.`);
+  socket.on("disconnect", () => {
+    //console.log(`${socket.id}: a user disconnected.`);
+  });
+});
--- chatbot_front/src/components/Navbar.jsx
+++ chatbot_front/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import CPE from "../assets/icons/cpe.png";
 import { IoMenu } from "react-icons/io5";
 import { SlClose } from "react-icons/sl";
+
+import Cookies from 'js-cookie';
+
+// new
+import axios from "axios";
+import React, { useEffect, useState, useContext } from "react";
+import { Title, Button, Flex } from "@mantine/core";
 
 const Navbar = () => {
   const navbarButton = [
@@ -37,6 +44,107 @@ const Navbar = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  // login
+  const queryParameters = new URLSearchParams(window.location.search);
+  const code = queryParameters.get("code");
+  // const navigate = useNavigate();
+
+  const [message, setMessage] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+
+  const setCookie = (name, studentId) => {
+    Cookies.set(name, studentId, { expires: 7, path: '/' });
+  };
+
+  const getUserInfo = () => {
+    const studentId = Cookies.get('studentId') ?? '';
+    const cmuAccount = Cookies.get('cmuAccount') ?? '';
+    const firstName = Cookies.get('firstName') ?? '';
+    const lastName = Cookies.get('lastName') ?? '';
+
+    const token = Cookies.get('token') ?? '';
+    
+    setUserInfo({
+      studentId,
+      cmuAccount,
+      firstName,
+      lastName,
+      token,
+    })
+  };
+
+  async function signIn(authorizationCode) {
+    console.log('ssss: ', `${process.env.REACT_APP_API_BASE_URL}/api/v1/cmuOAuth`)
+    try {
+      const resp = await axios.post(
+        `${process.env.REACT_APP_API_BASE_URL}/api/v1/cmuOAuth`,
+        {},
+        {
+          params: {
+            code: authorizationCode,
+          },
+          withCredentials: true,
+        }
+      );
+      console.log('test: ', resp)
+      console.log('resp.data: ', resp.data)
+      return resp.data;
+    } catch (err) {
+      if (!err.response) {
+        setMessage("Cannot connect to API Server. Please try again later.");
+      } else if (!err.response.data.ok) {
+        setMessage(err.response.data.message);
+      } else {
+        setMessage("Unknown error occurred. Please try again later.");
+      }
+    }
+  }
+
+  async function logout() {
+    Cookies.remove('studentId');
+    Cookies.remove('cmuAccount');
+    Cookies.remove('firstName');
+    Cookies.remove('lastName');
+    Cookies.remove('token');
+
+    window.location.href = '/'
+  }
+
+  useEffect(() => {
+    getUserInfo()
+  }, []);
+
+  useEffect(() => {
+    if (!code) return;
+
+    const fetchData = async () => {
+      const resp = await signIn(code);
+      if (resp) {
+        console.log('resp: ', resp)
+        setUserInfo(resp)
+        setCookie('studentId', resp.studentId)
+        setCookie('cmuAccount', resp.cmuAccount)
+        setCookie('firstName', resp.firstName)
+        setCookie('lastName', resp.lastName)
+        setCookie('token', resp.token)
+
+        window.location.href = '/'
+        // if (resp.itAccountType === "Admin") {
+        //   navigate("/admin-dashboard");
+        // } else if (resp.itAccountType === "StdAcc") {
+        //   navigate("/student-dashboard");
+        // } else if (resp.itAccountType === "MISEmpAcc") {
+        //   navigate("/instructor-dashboard");
+        // } else {
+        //   navigate("/");
+        // }
+      }
+    };
+
+    fetchData();
+  }, [code]);
+  
   return (
     <nav
       className={`flex justify-between fixed top-0 w-full lg:py-5 lg:px-24 px-10 py-3 text-white font-[Relax] z-50 duration-700 ${
@@ -60,11 +168,36 @@ const Navbar = () => {
         </div>
       </div>
       <div className="lg:flex hidden gap-3 select-none">
-        <button className="px-3 py-1 rounded-md border-2 border-white border-opacity-0 hover:border-opacity-100 duration-300">
+      <a 
+        className="px-3 py-1 rounded-md border-2 border-white border-opacity-0 hover:border-opacity-100 duration-300"
+        href='https://oauth.cmu.ac.th/v1/Authorize.aspx?response_type=code&client_id=sSGuKKdySjj0savuymnjDfAxP1aC6tmHVRcTTayn&redirect_uri=http://localhost:3000/cmuOAuthCallback&scope=cmuitaccount.basicinfo&state=xyz' >
+        <div
+          style={!userInfo.studentId ? {
+            boxShadow: "2px 2px 2px 2px rgba(0, 0, 0, 0.15)",
+            cursor: "pointer",
+          } : {display: "none"}}
+        >
           Sign In
-        </button>
-        <button className="px-3 py-1 rounded-md border-2 hover:bg-white/30 duration-300">
+        </div>
+      </a>
+        {/* <button 
+          className="px-3 py-1 rounded-md border-2 border-white border-opacity-0 hover:border-opacity-100 duration-300"
+          onClick={() => {handleOAuthLogin()}}
+        >
+          Sign In
+        </button> */}
+        <button 
+          className="px-3 py-1 rounded-md border-2 hover:bg-white/30 duration-300"
+          style={!userInfo.studentId ? {} : {display: "none"}}
+        >
           Sign Up
+        </button>
+        <button 
+          className="px-3 py-1 rounded-md border-2 hover:bg-white/30 duration-300"
+          style={userInfo.studentId ? {} : {display: "none"}}
+          onClick={logout}
+        >
+          Logout
         </button>
       </div>
       <button className="lg:hidden" onClick={handleNav}>
@@ -104,7 +237,10 @@ const Navbar = () => {
           ))}
           <div className="flex w-full justify-center items-center gap-5 mt-10">
             <div className="flex gap-3 select-none">
-              <button className="px-3 py-1 rounded-md border-2 border-white border-opacity-0 hover:border-opacity-100 duration-300">
+              <button 
+                className="px-3 py-1 rounded-md border-2 border-white border-opacity-0 hover:border-opacity-100 duration-300"
+                onClick={() => {}}
+              >
                 Sign In
               </button>
               <button className="px-3 py-1 rounded-md border-2 hover:bg-white/30 duration-300">
